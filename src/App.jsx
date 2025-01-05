93bb4ec321e34226947865322d31e8926c4b0a70
Hussam Alhambali <Alhanbali.hussam@hotmail.com>
2025-01-05 23:53:52 +0100

    Updated Public

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       </Routes>
 
       <img
-        src="/techover-logo.png"
+        src="./public/techover-logo.png"
         alt="logo"
         style={{ width: "200px", marginTop: "15px" }}
       />
